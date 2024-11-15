e7e282b49685aca72d15859fb714c2cc57954797
Naufal Denta <161413413+naufaldenta@users.noreply.github.com>
2024-11-15 10:09:35 +0700

    Update testing.cjs

--- testing/testing.cjs
+++ testing/testing.cjs
@@ -1,5 +1,5 @@
 const gemini = require('../src/index.cjs')
-gemini.setApiKey("AIzaSyDcV3AZ1elnKULDJvAOHjME0X9UBG8aw-4")
+gemini.setApiKey("GOOGLE_GEMINI_API")
 
 const runGemini = async () => {
     const result = await gemini.run("Buatkan aku langkah - langkah membuat pancake")
